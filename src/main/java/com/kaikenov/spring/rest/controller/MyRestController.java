80efde3a861ed56cf8ceae6ad903753d5615d430
Kaikenov Adilhan <adilhan_kai@mail.ru>
2020-12-30 15:14:08 +0600

    get employee

--- src/main/java/com/kaikenov/spring/rest/controller/MyRestController.java
+++ src/main/java/com/kaikenov/spring/rest/controller/MyRestController.java
@@ -4,6 +4,7 @@ import com.kaikenov.spring.rest.entity.Employee;
 import com.kaikenov.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,15 @@ public class MyRestController {
     @GetMapping("/employees")
     public List<Employee> showAllEmployee() {
         List<Employee> allEmployees = employeeService.getAllEmployees();
+
         return allEmployees;
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable("id") int id) {
+        Employee employee = employeeService.getEmployee(id);
+
+        return employee;
     }
 
 }
