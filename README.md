d6ae3e09f53793d3b4a00d5f2b3e7504ce5ed8e7
Kaikenov Adilhan <adilhan_kai@mail.ru>
2020-12-30 15:28:32 +0600

    Exception Handling

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ![spring-01](https://raw.githubusercontent.com/AdilhanKaikenov/spring_rest/master/etc/Spring_Rest.jpg)
 
 - Spring Java Configuration
+- Spring REST API Exception Handling
---
+++ src/main/java/com/kaikenov/spring/rest/exception_handling/EmployeeIncorrectData.java
@@ -0,0 +1,17 @@
+package com.kaikenov.spring.rest.exception_handling;
+
+public class EmployeeIncorrectData {
+
+    private String info;
+
+    public EmployeeIncorrectData() {
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
